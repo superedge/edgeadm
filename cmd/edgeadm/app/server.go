9f9974b37e3d777fc52dae6274cce773a6214b4b
dodiadodia <dodia@163.com>
2022-06-14 20:06:10 +0800

    Modify /tmp directory to /tmp/edgeadm-tmp/

--- cmd/edgeadm/app/server.go
+++ cmd/edgeadm/app/server.go
@@ -40,9 +40,9 @@ import (
 var (
 	edgeadmConf = cmd.EdgeadmConfig{
 		IsEnableEdge:   true,
-		WorkerPath:     "/tmp/",
+		WorkerPath:     "/tmp/edgeadm-tmp",
 		Kubeconfig:     "~/.kube/config",
-		ManifestsDir:   "/tmp/edge-manifests",
+		ManifestsDir:   "/tmp/edgeadm-tmp/edge-manifests",
 		InstallPkgPath: "",
 	}
 )
@@ -86,7 +86,7 @@ func globalFlagSet(flagset *flag.FlagSet) {
 	}
 
 	flagset.BoolVar(&edgeadmConf.IsEnableEdge, constant.ISEnableEdge, true, "Enable of install edge kubernetes cluster.")
-	flagset.StringVar(&edgeadmConf.WorkerPath, "worker-path", "/tmp/", "Worker path of install edge kubernetes cluster.")
+	flagset.StringVar(&edgeadmConf.WorkerPath, "worker-path", "/tmp/edgeadm-tmp/", "Worker path of install edge kubernetes cluster.")
 	flagset.StringVar(&edgeadmConf.Kubeconfig, "kubeconfig", "~/.kube/config", "The path to the kubeconfig file. [necessary]")
 
 	os.RemoveAll(edgeadmConf.WorkerPath)
